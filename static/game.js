ff0c5f4fc2ab4e103205e8cb8d15bf8fb460b442
Maritak1 <147379004+Maritak1@users.noreply.github.com>
2025-02-14 11:43:21 +0200

    sde

---
+++ static/game.js
@@ -0,0 +1,26 @@
+//no URL iegūst vārdu
+let adrese = window.location.hash;
+adrese = decodeURI(adrese);
+adrese = adrese.replace('#','');
+adrese = adrese.split(',');
+vards = adrese[0];
+
+//mainīgie spēles darbībai
+let laiks = 0
+let klikski = 0
+//masīvi spēles darbibai
+
+const laukumi = ['L01','L02','L03','L04','L05','L06','L07','L08','L09','L10','L11','L12']
+const laukumiSaturs = ['🌍','🐧','🍓','🌍','🌞','🐸','🌈','🍓','🌈','🌞','🐧','🐸']
+let atvertieLaukumi = []
+let pedejieDivi = []
+
+function  veiktGajienu(laukums)
+{
+    console.log('Klikšķis uz laukuma '+ laukums)
+    klikski++ //klikski = klikski + 1
+    let atvertsJaunsLaukums = false;
+    if (atvertieLaukumi.indexOf(laukums) == -1)
+    {atvertsJaunsLaukums = true;}
+    console.log('atverts jauns laukums')
+}
